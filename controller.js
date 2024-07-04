e993765e76973432c616d0f10d8243c4aa4bb330
luamy.gama@alunoesocolassociais.org.br <luamy.gama@alunoesocolassociais.org.br>
2024-07-04 09:02:11 -0300

    adicionando a função que chama o valor  para o acesso na tela de login

---
+++ controller.js
@@ -0,0 +1,6 @@
+//Validar acesso em tela de login
+
+    function acessar (){
+        let loginEmail = document.getElementById("loginEmail").value;
+        let loginSenha = document.getElementById("loginSenha").value;
+    }
